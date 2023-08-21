927c11792e4ec8418e342ee6591ffba7a6238aaf
Etherion <19214485+jjziets@users.noreply.github.com>
2023-08-21 08:26:51 +0200

    Update vastCollector.go

--- src/vastCollector.go
+++ src/vastCollector.go
@@ -26,4 +26,75 @@ func NewVastCollector(apiKey string) *VastCollector {
 			),
 			"earnings_total_gpu": prometheus.NewDesc(
 				"vastai_earnings_total_gpu",
-				"Total GPU earnings
+				"Total GPU earnings",
+				nil, nil,
+			),
+			"machines_total": prometheus.NewDesc(
+				"vastai_machines_total",
+				"Total number of machines",
+				nil, nil,
+			),
+			// Add more metrics here as needed
+		},
+		totalMachines: prometheus.NewDesc(
+			"vastai_machines_total",
+			"Total number of machines",
+			nil, nil,
+		),
+	}
+}
+
+func (c *VastCollector) Describe(ch chan<- *prometheus.Desc) {
+	for _, metric := range c.metrics {
+		ch <- metric
+	}
+	ch <- c.totalMachines
+}
+
+func (c *VastCollector) Collect(ch chan<- prometheus.Metric) {
+	// Fetch data from machineEarnings endpoint
+	earningsURL := fmt.Sprintf("https://console.vast.ai/api/v0/users/me/machine-earnings?api_key=%s", c.apiKey)
+	resp, err := http.Get(earningsURL)
+	if err != nil {
+		log.Printf("Error fetching machine earnings data: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var earningsData map[string]interface{}
+	err = json.Unmarshal(bodyBytes, &earningsData)
+	if err != nil {
+		log.Printf("Error decoding machine earnings data: %s", err)
+		return
+	}
+	log.Println("Successfully fetched machine earnings data.")
+	currentBalance := earningsData["current"].(map[string]interface{})["balance"].(float64)
+	totalGPU := earningsData["summary"].(map[string]interface{})["total_gpu"].(float64)
+
+	ch <- prometheus.MustNewConstMetric(c.metrics["earnings_current_balance"], prometheus.GaugeValue, currentBalance)
+	ch <- prometheus.MustNewConstMetric(c.metrics["earnings_total_gpu"], prometheus.GaugeValue, totalGPU)
+
+	// Fetch data from Machines endpoint
+	machinesURL := fmt.Sprintf("https://console.vast.ai/api/v0/machines?api_key=%s", c.apiKey)
+	resp, err = http.Get(machinesURL)
+	if err != nil {
+		log.Printf("Error fetching machines data: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var machinesData map[string][]interface{}
+	err = json.Unmarshal(bodyBytes, &machinesData)
+	if err != nil {
+		log.Printf("Error decoding machines data: %s", err)
+		return
+	}
+	log.Println("Successfully fetched machines data.")
+
+	totalMachines := float64(len(machinesData["machines"]))
+	ch <- prometheus.MustNewConstMetric(c.totalMachines, prometheus.GaugeValue, totalMachines)
+}
